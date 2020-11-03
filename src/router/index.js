24205551137165121e0ecce4ab52129587308441
Ayaka Neko <neko@ayaka.moe>
2020-11-03 17:22:04 +0800

    :sparkles: Added needed pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Landing from '../views/Landing.vue'
+import Exchange from '../views/Exchange.vue'
+import Assets from '../views/Assets.vue'
+import Transaction from '../views/Transaction.vue'
+import Analysis from '../views/Analysis.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +13,26 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Landing
+  },
+  {
+    path: '/exchange',
+    name: 'Exchange',
+    component: Exchange
+  },
+  {
+    path: '/assets',
+    name: 'Assets',
+    component: Assets
+  },
+  {
+    path: '/transactions',
+    name: 'Transaction',
+    component: Transaction
+  },
+  {
+    path: '/analysis',
+    name: 'Analysis',
+    component: Analysis
   }
 ]
 
