37f5a58cd7d92138197a46ee909fb1dffd3e46ce
Ayaka Neko <neko@ayaka.moe>
2020-11-04 17:01:37 +0800

    :sparkles: Added User info container

---
+++ src/components/User/Avatar.vue
@@ -0,0 +1,24 @@
+<template>
+    <el-avatar :size="size" :src="avatar"></el-avatar>
+</template>
+
+<script>
+export default {
+  props: {
+    avatar: {
+      type: String,
+      default: ''
+    },
+    size: {
+      type: Number,
+      default: 65
+    }
+  },
+  computed: {
+    remoteAvatar () {
+      const domain = ''
+      return domain + this.avatar
+    }
+  }
+}
+</script>
---
+++ src/components/User/UserInfoHeader.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="user-info-container">
+    <Avatar :avatar="user.avatar" />
+    <span class="username">{{ user.name }}</span>
+  </div>
+</template>
+
+<script>
+import Avatar from './Avatar.vue'
+
+export default {
+  components: {
+    Avatar
+  },
+  props: {
+    user: {
+      type: Object,
+      default: () => {}
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.user-info-container {
+  height: fit-content;
+  display: flex;
+  align-items: center;
+}
+
+.username {
+  margin-left: 15px;
+  font-weight: 500;
+}
+</style>
